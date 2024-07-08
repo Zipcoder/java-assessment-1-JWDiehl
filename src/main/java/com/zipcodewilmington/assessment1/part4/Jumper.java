41a863727b5efd42e0fbccb83186b92e92085a0d
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-08 15:54:12 -0400

    update as of 3:54

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,16 +6,18 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        // If j is greater than or equal to k, Tariq can reach the flag in one jump
-        if (j >= k) {
-            return 1;
-        }
 
         // Calculate full jumps of j units Tariq can make
         int fullJumps = k / j;
 
         //Calculate remaining height after full jumps of j units
         int remainingHeight = k % j;
+
+        // If j is greater than or equal to k
+        // Tariq can reach the flag in one jump
+        if (j >= k) {
+            return 1;
+        }
 
         //Calculate min jumps required
         if (remainingHeight == 0) {
