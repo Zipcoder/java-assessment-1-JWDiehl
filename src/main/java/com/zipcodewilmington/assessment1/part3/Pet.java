af5f7797a51ee1c69643c97c15dcb56ffbebe2a7
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-08 16:50:22 -0400

    Final Commit

--- src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Pet.java
@@ -16,7 +16,7 @@ public abstract class Pet implements Animal {
     private PetOwner owner;
 
     public Pet() {
-        this.name = "Cat name";
+        this.name = "Dog name";
         this.age = 0;
     }
 
